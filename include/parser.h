93a0351ddc5d92da6903fd56efeec615cd42eb3e
Betnix CEO <gstar3181@gmail.com>
2026-08-07 21:50:16 -0500

    Add parser.h for AST node definitions and parser class

---
+++ include/parser.h
@@ -0,0 +1,44 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include <vector>
+#include <string>
+#include <memory>
+
+// Forward declare tokens from lexer.c
+extern "C" {
+    #include "lexer.h"
+}
+
+// AST Node base structures
+struct ASTNode {
+    virtual ~ASTNode() = default;
+};
+
+struct VarDeclNode : public ASTNode {
+    std::string type;
+    std::string name;
+    std::string value;
+};
+
+struct PrintNode : public ASTNode {
+    std::string variableName;
+};
+
+class CPowerParser {
+private:
+    const char* sourceCursor;
+    int lineTracker;
+    int colTracker;
+    Token currentToken;
+
+    void advanceToken();
+    bool checkToken(TokenType type);
+    void consume(TokenType type, const std::string& errorMessage);
+
+public:
+    CPowerParser(const char* source);
+    std::vector<std::unique_ptr<ASTNode>> parseProgram();
+};
+
+#endif // PARSER_H
