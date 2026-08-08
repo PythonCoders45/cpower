5e7400b738bb664d1bbd6f171e0397469fd6187c
Betnix CEO <gstar3181@gmail.com>
2026-08-07 22:41:28 -0500

    Refactor variable storage to use RuntimeValue type

--- src/evaluator.hpp
+++ src/evaluator.hpp
@@ -4,10 +4,17 @@
 #include "parser.hpp"
 #include <unordered_map>
 #include <string>
+#include <variant>
+
+// A runtime value type that can hold numbers or strings
+using RuntimeValue = std::variant<int, double, std::string>;
 
 class Evaluator {
-    // Stores variables in memory: map of variable name -> value
-    std::unordered_map<std::string, std::string> variables;
+    // Memory map storing variable name -> value
+    std::unordered_map<std::string, RuntimeValue> variables;
+
+    RuntimeValue evaluateExpression(const std::string& expr);
+    std::string valueToString(const RuntimeValue& val);
 
 public:
     void execute(const std::unique_ptr<ASTNode>& node);
